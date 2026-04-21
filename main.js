ec7e020b7d9ba7f5862c3bd5d5648ecc6b334920
Coruppt <max.alpatov01@gmail.com>
2026-04-21 14:03:00 -0400

    fixed main.js

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-import { app, BrowserWindow, ipcMain, dialog, screen } from "electron";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
+const { app, BrowserWindow, ipcMain, dialog, screen } = require("electron");
+const fs = require("fs");
+const path = require("path");
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
